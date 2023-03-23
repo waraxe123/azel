00aa207b894d6207736edafa503686abcf65fc5b
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-24 06:56:40 +0700

    update __init__.py

--- Ubot/__init__.py
+++ Ubot/__init__.py
@@ -1,4 +1,19 @@
+import asyncio
+import logging
+import sys
+import time
+import os
+from datetime import datetime
+from logging.handlers import RotatingFileHandler
+from typing import Any, Dict
+from aiohttp import ClientSession
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from gpytranslate import Translator
+from pyrogram import Client, filters
+from pytgcalls import GroupCallFactory
+
 from config import *
-
-
 cmds = None
+CMD_HELP = {}
+clients = []
+ids = []
