15c94faecf6f6e004111148e09e95829b073d627
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-02 02:50:02 +0700

    update log.py

--- Azazel/modules/basic/log.py
+++ Azazel/modules/basic/log.py
@@ -27,7 +27,7 @@ LOG_CHATS_ = LOG_CHATS()
 async def monito_p_m_s(client, message):
     chat_id = message.chat.id
     user_id = client.me.id
-    botlog_chat_id = get_botlog(str(user_id))
+    botlog_chat_id = get_botlog(user_id)
     if gvarstatus(str(user_id), "PMLOG") and gvarstatus(str(user_id), "PMLOG") == "false":
         return
     if not no_log_pms_sql.is_approved(message.chat.id) and message.chat.id != 777000:
@@ -58,7 +58,7 @@ async def monito_p_m_s(client, message):
 async def log_tagged_messages(client, message):
     chat_id = message.chat.id
     user_id = client.me.id
-    botlog_chat_id = get_botlog(str(user_id))
+    botlog_chat_id = get_botlog(user_id)
     if gvarstatus(str(user_id), "GRUPLOG") and gvarstatus(str(user_id), "GRUPLOG") == "false":
         return
     if (no_log_pms_sql.is_approved(message.chat.id)):
