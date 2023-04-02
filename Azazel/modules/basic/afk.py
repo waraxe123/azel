28fe017a83042b71e74c3626144ee5e5874fa639
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-03 01:49:59 +0700

    update afk.py

--- Azazel/modules/basic/afk.py
+++ Azazel/modules/basic/afk.py
@@ -1,96 +1,128 @@
 
-from datetime import datetime
-from pyrogram import filters, Client
+import time
+from pyrogram import Client, filters
 from pyrogram.types import Message
-from Azazel.core.SQL.afksql import *
-from Azazel.core.SQL.botlogsql import *
-from ubotlibs.ubot.utils import get_text
+
+from Ubot.core import Types, get_message_type
+from ubotlibs.ubot.helper.parser import escape_markdown, mention_markdown
+from Ubot.core.SQL.botlogsql import *
+from Ubot.core.SQL.afk_db import *
 from . import *
 
+# Set priority to 11 and 12
+MENTIONED = []
+AFK_RESTIRECT = {}
+DELAY_TIME = 3  # seconds
 
-@Ubot("afk", "")
-async def set_afk(client, message):
-    if len(message.command) == 1:
-        return await message.reply(f"**Gunakan format dengan berikan alasan**\n\n**Contoh** : `afk berak`")
-    user_id = client.me.id
-    botlog = get_botlog(str(user_id))
-    ajg = await message.edit("Processing..")
-    msge = None
-    msge = get_text(message)
-    start_1 = datetime.now()
-    afk_start = start_1.replace(microsecond=0)
-    if msge:
-        msg = f"**❏ Sedang AFK**.\n** ╰ Alasan** : `{msge}`"
-        await client.send_message(botlog, afkstr.format(msge))
-        set_afk(user_id, afk_start, msge)
-    else:
-        msg = "**❏ Sedang AFK**."
-        await client.send_message(botlog, afkstr.format(msge))
-        set_afk(user_id, afk_start)
-    await ajg.edit(msg)
 
-@Client.on_message(
-    is_afk
-    & (filters.mentioned | filters.private)
-    & ~filters.me
-    & ~filters.bot
-    & filters.incoming
-)
-async def afk_er(client, message):
+@Client.on_message(filters.me & filters.command("afk", ""))
+async def afk(client, message):
     user_id = client.me.id
-    if not message:
-        return
-    if not message.from_user:
-        return
-    if message.from_user.id == user_id:
-        return
-    use_r = int(user_id)
-    if use_r not in afk_sanity_check.keys():
-        afk_sanity_check[use_r] = 1
-    else:
-        afk_sanity_check[use_r] += 1
-    if afk_sanity_check[use_r] == 5:
-        await message.reply_text(
-            "**❏ Sedang AFK**."
+    if len(message.text.split()) >= 2:
+        set_afk(str(user_id), True, message.text.split(None, 1)[1])
+        await message.edit(
+            "<b>❏ {} Sedang AFK</b>\n╰  <b>Alasan:</b> <code>{}</code>".format(
+                mention_markdown(message.from_user.id, message.from_user.first_name),
+                message.text.split(None, 1)[1],
+            )
         )
-        afk_sanity_check[use_r] += 1
-        return
-    if afk_sanity_check[use_r] > 5:
-        return
-    bsgt = check_afk_status(user_id)
-    reason = bsgt["reason"]
-    if reason == "":
-        reason = None
-    back_alivee = datetime.now()
-    afk_start = bsgt["time"]
-    afk_end = back_alivee.replace(microsecond=0)
-    total_afk_time = str((afk_end - afk_start))
-    message_to_reply = (
-        f"**❏ Sedang AFK**\n** ├ Waktu** :`{total_afk_time}`\n** ╰ Alasan** : `{reason}`"
-        if reason
-        else f"**❏ Sedang AFK**\n** ╰ Waktu** :`{total_afk_time}`"
-    )
-    await message.reply(message_to_reply)
-    
+    else:
+        set_afk(str(user_id), True, "")
+        await message.edit(
+            "<b>❏ {} Sedang AFK</b>".format(
+                mention_markdown(message.from_user.id, message.from_user.first_name)
+            )
+        )
+    await message.stop_propagation()
 
-@Client.on_message(filters.outgoing & filters.me & is_afk)
-async def no_afke(client, message):
+
+@Client.on_message(filters.mentioned & ~filters.bot, group=11)
+async def afk_mentioned(client: Client, message: Message):
+    global MENTIONED
     user_id = client.me.id
-    botlog = get_botlog(str(user_id))
-    nyet = check_afk_status(user_id)
-    back_alivee = datetime.now()
-    afk_start = nyet["time"]
-    afk_end = back_alivee.replace(microsecond=0)
-    total_afk_time = str((afk_end - afk_start))
-    kk = await message.reply(f"**❏ Saya Kembali.**\n** ╰ AFK Selama** : {total_afk_time}")
-    await kk.delete()
-    rm_afk(user_id)
-    await client.send_message(botlog, onlinestr.format(total_afk_time))
+    botlog_group_id = get_botlog(str(user_id))
+    get = get_afk(user_id)
+    if get and get["afk"]:
+        if "-" in str(message.chat.id):
+            cid = str(message.chat.id)[4:]
+        else:
+            cid = str(message.chat.id)
+
+        if cid in list(AFK_RESTIRECT):
+            if int(AFK_RESTIRECT[cid]) >= int(time.time()):
+                return
+        AFK_RESTIRECT[cid] = int(time.time()) + DELAY_TIME
+        if get["reason"]:
+            await message.reply(
+                "<b>❏ Sedang AFK</b>\n<b>╰ Alasan:</b> <code>{}</code>".format(
+                    client.me.mention, get["reason"]
+                )
+            )
+        else:
+            await message.reply(
+                f"<b>Maaf</b> {client.me.first_name} <b>Sedang AFK</b>"
+            )
+
+        _, message_type = get_message_type(message)
+        if message_type == Types.TEXT:
+            if message.text:
+                text = message.text
+            else:
+                text = message.caption
+        else:
+            text = message_type.name
+
+        MENTIONED.append(
+            {
+                "user": message.from_user.first_name,
+                "user_id": message.from_user.id,
+                "chat": message.chat.title,
+                "chat_id": cid,
+                "text": text,
+                "message_id": message.message_id,
+            }
+        )
+        await client.send_message(
+            botlog_group_id,
+            "<b>#MENTION\n • Dari :</b> {}\n • <b>Grup :</b> <code>{}</code>\n • <b>Pesan :</b> <code>{}</code>".format(
+                message.from_user.mention,
+                message.chat.title,
+                text[:3500],
+            ),
+        )
+
+
+@Client.on_message(filters.me & filters.group, group=12)
+async def no_longer_afk(client: Client, message: Message):
+    global MENTIONED
+    user_id = client.me.id
+    botlog_group_id = get_botlog(str(user_id))
+    get = get_afk(str(user_id))
+    if get and get["afk"]:
+        await client.send_message(botlog_group_id, "Anda sudah tidak lagi AFK")
+        set_afk(str(user_id), False, "")
+        text = "<b>Total {} Mention Saat Sedang AFK<b>\n".format(len(MENTIONED))
+        for x in MENTIONED:
+            msg_text = x["text"]
+            if len(msg_text) >= 11:
+                msg_text = "{}...".format(x["text"])
+            text += "- [{}](https://t.me/c/{}/{}) ({}): {}\n".format(
+                escape_markdown(x["user"]),
+                x["chat_id"],
+                x["message_id"],
+                x["chat"],
+                msg_text,
+            )
+        await client.send_message(botlog_group_id, text)
+        MENTIONED = []
+
 
 add_command_help(
     "Afk",
     [
-        [f"afk","Mengaktifkan mode afk.",
+        [
+            "afk <alasan>",
+            "Memberi tahu orang yang menandai atau membalas salah satu pesan atau dm anda kalau anda sedang afk",
         ],
     ],
 )
