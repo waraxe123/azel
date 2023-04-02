73f3b6a6da6d0359ebdc72d45fcc56c0b2ff2a2d
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-03 00:31:38 +0700

    update __init__.py

--- Azazel/__init__.py
+++ Azazel/__init__.py
@@ -9,7 +9,7 @@ from typing import Any, Dict
 from aiohttp import ClientSession
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from gpytranslate import Translator
-from pyrogram import Client, filters
+from pyrogram import Client, filters, __version__, enums
 from pytgcalls import GroupCallFactory
 from .logging import *
 from config import *
