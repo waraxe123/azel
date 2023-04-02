92d06e96dd469b74f70d57c8077bd839dd20cd1f
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-02 17:01:18 +0700

    update botlogsql.py

--- Azazel/core/SQL/botlogsql.py
+++ Azazel/core/SQL/botlogsql.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, String, Integer, BigInteger
 from . import BASE, SESSION
 
 class BotLog(BASE):
-    __tablename__ = "botlog_chatid"
+    __tablename__ = "botlog"
     user_id = Column(String(14), primary_key=True)
     group_id = Column(BigInteger, nullable=False)
 
@@ -12,6 +12,27 @@ class BotLog(BASE):
 
 BotLog.__table__.create(checkfirst=True)
 
+
+async def buat_log(bot):
+    user = await bot.get_me()
+    botlog = SESSION.query(BotLog).filter(BotLog.user_id == user_id).first()
+
+    if botlog:
+        botlog.group_id = int(group_id)
+    else:
+        group_name = 'Azazel Project Bot Log'
+        group_description = 'Jangan Hapus Atau Keluar Dari Grup Ini\n\nCreated By @AzazelProjectBot.\nJika menemukan kendala atau ingin menanyakan sesuatu\nHubungi : @KynanSupport.'
+        group = await bot.create_supergroup(group_name, group_description)
+        botlog.group_id = int(group_id)
+        text = 'Grup Log Berhasil Dibuat,\nKetik `id` untuk mendapatkan id log grup\nKemudian ketik `setlog` ID_GROUP\n\nContoh : setlog -100749492984'
+        await bot.send_message(botlog.group_id, text)
+
+        adder = BotLog(user_id=user_id, group_id=group_id)
+        SESSION.add(adder)
+        SESSION.commit()
+
+    SESSION.close()
+    return botlog
 
 def get_botlog(user_id):
     try:
