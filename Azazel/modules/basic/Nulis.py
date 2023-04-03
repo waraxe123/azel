cf48a0626822e4ede2a8864700824d4e10f99dab
ayrizz <118755946+ayrizz@users.noreply.github.com>
2023-04-03 19:57:43 +0700

    Create Nulis.py

---
+++ Azazel/modules/basic/Nulis.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from . import *
+import requests
+
+@Ubot("nulis", "")
+async def handwrite(client, message):
+    if message.reply_to_message:
+        monyet = message.reply_to_message.text
+    else:
+        monyet = message.text.split(None, 1)[1]
+    babi = await message.reply("`Processing...`")
+    ajg = requests.get(f"https://api.sdbots.tk/write?text={naya}").url
+    await message.reply_photo(
+        photo=ajg,
+        caption=f"**Ditulis Oleh :** {client.me.mention}")
+    await babi.delete()
+
+
+add_command_help(
+    "Nulis",
+    [
+        [f"nulis [berikan pesan/balas ke pesan]", "Membuat Tulisan Untuk Kamu Yang Malas Nulis."],
+    ],
+)
