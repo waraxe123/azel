b9446bf948ab8e6ca4cdb3b5491f70c9f846834a
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-03 19:04:33 +0700

    update help.py

--- Azazel/modules/basic/help.py
+++ Azazel/modules/basic/help.py
@@ -67,7 +67,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"**Bantuan {str(help_arg).upper()}**\n\n"
             for x in commands:
                 this_command += f"๏ **Keterangan:** `{str(commands[x])}`\n\n"
-            this_command += ""
+            this_command += "@KynanSupport"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
